a61fac4b521711494865a1ac0c78cd5ee2f4b346
docksidealexandria <b.holms272@gmail.com>
2026-01-12 11:17:42 -0600

    Create page.jsx

---
+++ page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Dockside Alexandria</h1>
+      <p>Lakefront vacation rental in Alexandria, MN</p>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
