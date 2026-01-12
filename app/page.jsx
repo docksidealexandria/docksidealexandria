81f573fd15f1d1a95db6084d5832d1b5028f8913
docksidealexandria <b.holms272@gmail.com>
2026-01-12 11:48:26 -0600

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Dockside Alexandria</h1>
-      <p>Lakefront vacation rental in Alexandria, MN</p>
-      <p>Coming soon.</p>
-    </main>
-  );
-}
