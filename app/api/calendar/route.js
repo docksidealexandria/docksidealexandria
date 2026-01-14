be9a9e54b0e00868b905f4f0d8aefe7cf44a8c88
docksidealexandria <b.holms272@gmail.com>
2026-01-14 10:06:37 -0600

    Update route.js

--- app/api/calendar/route.js
+++ app/api/calendar/route.js
@@ -2,7 +2,7 @@ import ical from "node-ical";
 
 export async function GET() {
   const AIRBNB_ICAL =
-    "https://www.airbnb.com/calendar/ical/YOUR_LISTING_ID.ics";
+    "https://www.airbnb.com/calendar/ical/1345046922187021169.ics?t=d942422183e145ba83947acffaf36df9";
 
   const data = await ical.fromURL(AIRBNB_ICAL);
 
